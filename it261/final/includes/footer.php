75e44e6259b565e1b5ed6ad5165c8385a0255465
Helene Liobard <77423175+Helene-33@users.noreply.github.com>
2021-12-16 16:47:41 -0800

    upload address check code in footer

--- it261/final/includes/footer.php
+++ it261/final/includes/footer.php
@@ -3,7 +3,7 @@
 <li>Copyright <?php echo date('Y');?></li>
  <li>All Rights Reserved</li>
 <li><a href="../../index.php" target="_blank">Web Design by Helene</a></li>
-<li><a href="https://validator.w3.org/nu/?doc=https%3A%2F%2Fwww.ellencodes.com%2Fit261%2Fweeks%2Fweek9%2Findex.php" target="_blank">HTML Validation</a></li>
-<li><a href="https://jigsaw.w3.org/css-validator/validator?uri=https://www.ellencodes.com/it261/weeks/week9/index.php" target="_blank">CSS Validation</a></li>
+<li><a href="https://validator.w3.org/nu/?doc=https%3A%2F%2Fwww.ellencodes.com%2Fit261%2Ffinal%2Findex.php" target="_blank">HTML Validation</a></li>
+<li><a href="https://jigsaw.w3.org/css-validator/validator?uri=https://www.ellencodes.com/it261/final/index.php" target="_blank">CSS Validation</a></li>
 </ul>
 </footer>
